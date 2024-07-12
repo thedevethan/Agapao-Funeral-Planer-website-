2e3e58294f29caffcc09adc73e81dc8b2f490da0
thedevethan <170204507+thedevethan@users.noreply.github.com>
2024-07-12 16:52:00 +0100

    Navbar computer and phone infinity ok

--- php/navbar.php
+++ php/navbar.php
@@ -1,16 +1,29 @@
-<nav>
-    <img src="image/Agapao - logo.png" alt="logo" class="logo">
+<div class="navbar">
+    <nav>
+        <img src="image/Agapao - logo.png" alt="logo" class="logo">
 
-    <script>
-        $(document).ready(function() {
-            $(".logo").click(function() {
-                window.location = "index.php";
+        <script>
+            $(document).ready(function() {
+                $(".logo").click(function() {
+                    window.location = "index.php";
+                });
             });
-        });
-    </script>
+        </script>
 
 
-    <div class="nav-link">
+        <div class="nav-link">
+            <ul>
+                <li><a href="#">Prestations & services</a></li>
+                <li><a href="#">À propos d’Agapao-Funeral</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a class="contact" href="#">Contact</a></li>
+            </ul>
+        </div>
+
+        <img src="image/menu.png" alt="menu" class="hamburger">
+    </nav>
+
+    <div class="nav-link-mobile">
         <ul>
             <li><a href="#">Prestations & services</a></li>
             <li><a href="#">À propos d’Agapao-Funeral</a></li>
@@ -19,28 +32,17 @@
         </ul>
     </div>
 
-    <img src="image/menu.png" alt="menu" class="hamburger">
-</nav>
-
-<div class="nav-link-mobile">
-    <ul>
-        <li><a href="#">Prestations & services</a></li>
-        <li><a href="#">À propos d’Agapao-Funeral</a></li>
-        <li><a href="#">Blog</a></li>
-        <li><a class="contact" href="#">Contact</a></li>
-    </ul>
+    
 </div>
-
-
+<p>AHHHHHHHHHHHHHHHH</p>
 
 <script>
     $(document).ready(function() {
-        var targetElement = $("#targetElement");
 
         function nav_scroll(padding_end, padding_start, logo_height_end, logo_height_start) {
 
             $(window).scroll(function() {
-                
+
                 var scroll = $(window).scrollTop();
 
                 if (scroll > 0) {
@@ -49,18 +51,25 @@
                         "height": logo_height_end,
                     });
 
+                    $(".nav-link-mobile").css({
+                        "background-color": "rgba(255, 255, 255, 0.721)",
+                    })
+
                     $("nav").css({
                         "background-color": "rgba(255, 255, 255, 0.721)",
-
                         "transition": "0.3s",
-                        "padding": padding_end          
+                        "padding": padding_end
                     });
                 } else {
                     $("nav").css({
                         "background-color": "rgb(255, 255, 255)",
                         "transition": "0.3s",
-                        "padding":  padding_start  
+                        "padding": padding_start
                     });
+
+                    $(".nav-link-mobile").css({
+                        "background-color": "rgb(255, 255, 255)",
+                    })
 
                     $(".logo").css({
                         "transition": "0.3s",
@@ -79,14 +88,26 @@
             if (windowWidth <= 600) {
                 // Animation pour téléphones
                 nav_scroll("1vh 3vw", "3vh 3vw", "10vw", "14vw");
-                
+
+                $(".hamburger").click(function(){
+
+                    if ($(".nav-link-mobile").is(':visible')) {
+                        this.setAttribute('src','image/menu.png')
+                        $(".nav-link-mobile").slideToggle()
+                    }
+                    else {
+                        this.setAttribute('src','image/close.png')
+                        $(".nav-link-mobile").slideToggle()
+                    };
+                });
+
             } else if (windowWidth <= 1024) {
                 // Animation pour tablettes
                 targetElement.fadeToggle();
             } else {
                 // Animation pour grands écrans (ne pas appliquer d'animation)
-                nav_scroll("0.3vh 2vw", "0.5vh 2vw", "5vw", "6.5vw");
-            }
+                nav_scroll("0.3vh 2vw", "0.5vh 2vw", "5vw", "6.5vw")
+            };
         }
 
         // Événement au clic sur le bouton
