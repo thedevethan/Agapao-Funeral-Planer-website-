6b06429b44386d27a83c484551c117559ecc9fe8
thedevethan <170204507+thedevethan@users.noreply.github.com>
2024-07-15 23:19:58 +0100

    Début de la page home hébergée

--- php/header.php
+++ php/header.php
@@ -32,8 +32,8 @@ function hd($style, $icon)
         <!--AOS animations-->
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
+        <!--Hover.css-->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hover.css/2.3.1/css/hover-min.css">
-
 
         <!--Title-->
         <title>Agapao-Funeral-Planer</title>
--- php/home.php
+++ php/home.php
@@ -15,7 +15,7 @@
 
         <div class="left">
             <p id="p1" data-aos="fade-up" data-aos-duration="1000" data-aos-easing="ease-out">ACCOMPAGNEMENT FUNÉRAIRE ET CÉRÉMONIES D'OBSÈQUES PERSONNALISÉES EN CÔTE D'IVOIRE.</p>
-            <p id="p2" data-aos="fade-up" data-aos-duration="1400" data-aos-easing="ease-out">Honorer la vie, alléger votre fardeau avec respect et dignité.</p>
+            <p id="p2" data-aos="fade-up" data-aos-duration="1400" data-aos-easing="ease-out">Honorer la vie, alléger votre fardeau avec respect et dignité</p>
             <p id="p3" data-aos="fade-up" data-aos-duration="1800" data-aos-easing="ease-out" data-aos-anchor="#p2"><span class="bold">Agapao Funeral Planner</span> est une agence spécialisée dans <span class="bold">l'organisation</span>
                 et la <span class="bold">planification</span> d'obsèques personnalisées qui respecte les volontés du défunt,
                 vos volontés en apportant soutien et réconfort aux familles endeuillées.
@@ -26,9 +26,20 @@
             <a href="" class="link hvr-bounce-to-top">Découvrir les prestations</a>
             <img src="image/prestation.jpg" alt="prestation" class="background-image2">
         </div>
-    </div>
 
+    </div>
 
 </div>
 
+
+<div class="div2" data-aos="zoom-in" data-aos-duration="1400">
+    <div class="div1">
+        
+        <div class="right-mobile">
+            <a href="" class="link hvr-bounce-to-top">Découvrir les prestations</a>
+        </div>
+        <div class="green1"></div>
+    </div>
+
+    <div class="green2"></div>
 </div>
