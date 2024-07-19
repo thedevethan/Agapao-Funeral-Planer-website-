a6fe0269a5d24c404b774256bc0dbe51c956c105
thedevethan <170204507+thedevethan@users.noreply.github.com>
2024-07-19 20:42:00 +0100

    vision et valeurs refaites computer

--- php/home.php
+++ php/home.php
@@ -35,6 +35,137 @@
 
 </div>
 
+<div class="body-menu">
+    <div class="vision">
+        <div class="title">
+            <p>
+                NOTRE VISION
+            </p>
+            <img src="image/floral1.png" alt="flower">
+        </div>
+        <div class="bodyy">
+            <div class="g2">
+                <div class="g1">
+                    <div class="vision-p">
+                        <p id="p2">
+                            Notre agence se distingue par son <span class="gold">professionnalisme</span>, son <span class="gold">engagement</span> à être un <span class="gold">partenaire</span> <span class="gold">empathique</span>, respectueux des <span class="gold">traditions</span> et de vos <span class="gold">désirs</span>. Elle repose sur des <span class="gold">valeurs</span> <span class="gold">bienveillantes</span> et des <span class="gold">prestations</span> de <span class="gold">qualité</span>.
+                            Nous sommes totalement disposés à vous guider dans l’<span class="gold">organisation</span> de <span class="gold">funérailles</span> personnalisées. Dès la <span class="gold">constatation</span> du <span class="gold">décès</span>, à votre <span class="gold">sollicitation</span>, nous nous engageons à créer un <span class="gold">hommage</span> <span class="gold">sur-mesure</span> et faire une <span class="gold">gestion</span> complète des <span class="gold">obsèques</span>…
+                            Nos <span class="gold">prestations</span> sont <span class="gold">accessibles</span>, et capables de répondre aux <span class="gold">besoins</span> des <span class="gold">familles</span> <span class="gold">endeuillées</span>. Nous offrons à nos <span class="gold">clients</span> une <span class="gold">gamme</span> de <span class="gold">prestations</span> <span class="gold">diverses</span> et <span class="gold">personnalisées</span>, pour des <span class="gold">cérémonies</span> qui <span class="gold">honorent</span> la <span class="gold">vie</span>, l'<span class="gold">amour</span> et l'<span class="gold">héritage</span> du <span class="gold">défunt</span> sans jamais compromettre la <span class="gold">qualité</span> ou la <span class="gold">dignité</span> de ce dernier <span class="gold">hommage</span>.
+                        </p>
+                    </div>
+                    <div class="vert1"></div>
+                </div>
+                <div class="vert2"></div>
+            </div>
+        </div>
+    </div>
+    <div class="value">
+        <div class="title">
+            <p>
+                NOS VALEURS
+            </p>
+            <img src="image/floral1.png" alt="flower">
+        </div>
+
+        <div class="bodyyy">
+            <div class="home" >
+                <p id="title">
+                    ACCUEIL
+                </p>
+                <img src="image/hand-shake.png" alt="hand">
+                <p id="text">
+                    Chez <span class="gold">Agapao Funeral Planner</span>, <span class="gold">l'acceuil</span> est une <span class="gold">philosophie</span> qui guide notre approche
+                    et nos interactions avec chaque famille. Nous vous offrons un accueil <span class="gold">chaleureux</span>,
+                    <span class="gold">empathique</span> et <span class="gold">respectueux</span> pour vous accompagner dans ce moment difficile.
+                </p>
+            </div>
+
+            <div class="pro">
+                <p id="title">
+                    PROFESSIONNALISME
+                </p>
+                <img src="image/briefcase2.png" alt="bag">
+                <p id="text">
+                    Notre <span class="gold">professionnalisme</span> est le gage de la <span class="gold">qualité</span> du service que nous vous devons.
+                    Nous concevons et mettons à votre disposition une gamme de prestations suivant
+                    les <span class="gold">meilleurs standards</span> en matière d'<span class="gold">organisation</span> de cérémonie funéraire avec une équipe
+                    <span class="gold">professionnelle</span> et <span class="gold">respectueuse</span>.
+                </p>
+            </div>
+
+            <div class="ecoute">
+            <p id="title">
+                ÉCOUTE
+            </p>
+            <img src="image/ear.png" alt="hear">
+            <p id="text">
+                L'<span class="gold">écoute</span> nous permet d'avoir une vision <span class="gold">objective</span> et <span class="gold">précise</span> de votre besoin.
+                Nous vous écoutons avec une <span class="gold">ouverture d'esprit</span> pour vous <span class="gold">comprendre</span> et vous apporter
+                l'assistance la plus <span class="gold">adaptée</span> pour vous et vos proches.
+            </p>
+            </div>
+
+            <div class="dispo" >
+            <p id="title">
+                DISPONIBILITÉ
+            </p>
+            <img src="image/calendar.png" alt="calendar">
+            <p id="text">
+            Nous <span class="gold">sommes</span> présents auprès de nos <span class="gold">clients</span>
+            et des <span class="gold">familles</span> à toutes les étapes des <span class="gold">obsèques</span>.
+            Notre <span class="gold">implication</span> traduit notre <span class="gold">disponibilité</span> à vos côtés afin de 
+            <span class="gold">supporter</span> mais surtout de vous <span class="gold">décharger</span> dans cette 
+            <span class="gold">période</span>.
+            </p>
+            </div>
+
+            <div class="conf" >
+            <p id="title">
+                CONFIDENTIALITÉ
+            </p>
+            <img src="image/private-account.png" alt="conf">
+            <p id="text">
+            Nous <span class="gold">sommes</span> présents auprès de nos <span class="gold">clients</span> et des <span class="gold">familles</span> à toutes les étapes des <span class="gold">obsèques</span>. Notre <span class="gold">implication</span> traduit notre <span class="gold">disponibilité</span> à vos côtés afin de <span class="gold">supporter</span> mais surtout de vous <span class="gold">décharger</span> dans cette <span class="gold">période</span>.
+            </p>
+            </div>
+        </div>
+        <div class="prestations">
+        <div class="title">
+            <p>
+                PRESTATIONS & ACCOMPAGNEMENTS
+            </p>
+            <img src="image/floral1.png" alt="flower">
+        </div>
+    </div>
+    <div class="event">
+        <div class="title">
+            <p>
+                EVENEMENTS
+            </p>
+            <img src="image/floral1.png" alt="flower">
+        </div>
+    </div>
+    <div class="article">
+        <div class="title">
+            <p>
+                ARTICLES
+            </p>
+            <img src="image/floral1.png" alt="flower">
+        </div>
+    </div>
+    <div class="galerie">
+        <div class="title">
+            <p>
+                GALERIE
+            </p>
+            <img src="image/floral1.png" alt="flower">
+        </div>
+    </div>
+    </div>
+
+</div>
+
+<!--
 <div class="div2" data-aos="zoom-in" data-aos-duration="1400" data-aos-once="true">
     <div class="div1">
 
@@ -48,16 +179,16 @@
 </div>
 
 <div class="decoration-flower1">
-    <img src="image/floral1.png" alt="flower1" class="background-image3">
+    <p>
+        NOTRE VISION
+    </p>
+    <img src="image/floral1.png" alt="flower1" class="background-image3 center-to-outer" data-aos="center-to-outer">
 </div>
 
 <div class="valeurs">
     <div class="g2" data-aos="fade-right" data-aos-once="true">
         <div class="g1">
             <div class="pourquoi">
-                <p id="p1">
-                    NOTRE VISION
-                </p>
                 <p id="p2">
                     Notre agence se distingue par son <span class="gold">professionnalisme</span>, son <span class="gold">engagement</span> à être un <span class="gold">partenaire</span> <span class="gold">empathique</span>, respectueux des <span class="gold">traditions</span> et de vos <span class="gold">désirs</span>. Elle repose sur des <span class="gold">valeurs</span> <span class="gold">bienveillantes</span> et des <span class="gold">prestations</span> de <span class="gold">qualité</span>.
                     Nous sommes totalement disposés à vous guider dans l’<span class="gold">organisation</span> de <span class="gold">funérailles</span> personnalisées. Dès la <span class="gold">constatation</span> du <span class="gold">décès</span>, à votre <span class="gold">sollicitation</span>, nous nous engageons à créer un <span class="gold">hommage</span> <span class="gold">sur-mesure</span> et faire une <span class="gold">gestion</span> complète des <span class="gold">obsèques</span>…
@@ -70,7 +201,12 @@
         <div class="vert2"></div>
     </div>
 
-
+    <div class="decoration-flower2">
+        <p>
+            NOTRE VISION
+        </p>
+        <img src="image/floral1.png" alt="flower1">
+    </div>
 
     <div class="valeurs-content">
         <div class="top">
@@ -127,3 +263,4 @@
 
     <div class="green2"></div>
 </div>
+-->
